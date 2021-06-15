aef87fcb320f21ac1fc1560f39640410f31f3077
MatokhinDanil <85947848+MatokhinDanil@users.noreply.github.com>
2021-06-15 19:43:18 +0300

    Add files via upload

---
+++ dz3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int num1;
+	float num2;
+
+	std::cout << "Enter two numbers:";
+	std::cin >> num1 >> num2;
+
+	float prod = num1 * num2;
+	float div = num1 / num2;
+
+	std::setprecision(7);
+	std::cout  << "Prod=" << prod << std::endl
+		       << "div=" << div << std::endl;
+	return 0;
+
+}
---
+++ №2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip> // чтобы работал setprecision
+
+/*
+ Вариант 12, периметр и площадь квадрата (номер 2с)
+*/
+
+float num1;
+
+void per() {
+	std::cout << "Perimetr =" << num1 + num1 <<std::endl ;
+}
+void plosh() {
+	std::cout << "Ploshad =" << num1 * num1 <<std::endl ;
+}
+
+int main()
+{
+
+	std::ifstream fin;
+	std::ofstream fout;
+	fin.open("in.txt");
+	fout.open("out.txt");
+
+
+	std::cout << "Введите сторону квадрата: ";
+	std::cin >>  num1;
+	fin >>  num1;
+	fin.close();
+
+	per();
+	plosh();
+
+	fout.close();
+
+
+	return 0;
+
+}
